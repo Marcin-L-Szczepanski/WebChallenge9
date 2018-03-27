0d4fe0b209506ff364a85c258d14aa40141327dc
Marcin Szczepanski <marcin.l.szczepanski@gmail.com>
2018-03-27 14:55:35 +0900

    Add CSS loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,24 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   entry: './src/scripts/main.js',
+  
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.bundle.js'
-  }
+  },
+  
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract('css-loader')
+      }
+    ]
+  },
+
+  plugins: [
+    new ExtractTextPlugin('main.css')
+  ]
 }
