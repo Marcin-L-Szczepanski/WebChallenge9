63cdcab6344b9b3bf1869fb3727fbb641e2b721f
Marcin Szczepanski <marcin.l.szczepanski@gmail.com>
2018-05-20 18:18:06 +0900

    Open menu function

--- src/scripts/Menu.js
+++ src/scripts/Menu.js
@@ -12,6 +12,22 @@ function menuHidable() {
 function createMenuBtn() {
   const menuBtn = document.createElement('button');
   menuBtn.classList.add('menu__button');
-  menuBtn.innerHTML = "Open Menu"
+  menuBtn.innerHTML = 'Open Menu';
+  menuBtn.addEventListener('click', openCloseMenu);
   menu.insertBefore(menuBtn, menu.childNodes[0]);
+}
+
+function openCloseMenu() {
+  const menuItems = document.getElementById('menu-items');
+  if (menuItems.classList.contains('menu__items--closed')) {
+    menuItems.classList.remove('menu__items--closed');
+    menuItems.classList.add('menu__items--open'); 
+    this.classList.add('menu__button--menuOpen');
+    this.innerHTML = 'Close Menu';
+  } else {
+    menuItems.classList.remove('menu__items--open');
+    menuItems.classList.add('menu__items--closed'); 
+    this.classList.remove('menu__button--menuOpen');
+    this.innerHTML = 'Open Menu';
+  };
 }
