933fb697a3094818575e8ab4832ab66b56e96140
Marcin Szczepanski <marcin.l.szczepanski@gmail.com>
2018-05-21 21:54:06 +0900

    Add 'siema' slider

--- src/scripts/vendor/siema.js
+++ src/scripts/vendor/siema.js
@@ -1,2 +1,15 @@
 import Siema from 'siema';
-new Siema();
+new Siema({
+  selector: '.opinions__slider',
+  duration: 200,
+  easing: 'ease-out',
+  perPage: 1,
+  startIndex: 1,
+  draggable: true,
+  multipleDrag: true,
+  threshold: 20,
+  loop: false,
+  rtl: false,
+  onInit: () => {},
+  onChange: () => {},
+});
