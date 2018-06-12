a4a7ef3a4842d007d1b4a863d565ddb370c43491
Marcin Szczepanski <marcin.l.szczepanski@gmail.com>
2018-06-13 07:00:09 +0900

    Fix smooth scroll

--- src/scripts/Hero-banner.js
+++ src/scripts/Hero-banner.js
@@ -2,5 +2,5 @@
 
 const scrollDown = document.getElementById('scroll-down');
 scrollDown.addEventListener('click', function() {
-  window.scrollBy({ top: (window.innerHeight - window.pageYOffset), left: 0, behavior: 'smooth' });
+  window.scrollBy({ top: (window.innerHeight - window.pageYOffset) + 1, left: 0, behavior: 'smooth' });
 });
